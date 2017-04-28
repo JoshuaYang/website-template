95ccc770267904105ca897f1eb9b03a2d59a36d7
JoshuaYang <yss901025@gmail.com>
2017-04-28 16:35:56 +0800

    remove dist when build

--- conf/webpack.prod.config.js
+++ conf/webpack.prod.config.js
@@ -2,12 +2,18 @@ const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const baseWebpackConfig = require('./webpack.base.config.js');
 
 module.exports = merge(baseWebpackConfig, {
     plugins: [
+        new CleanWebpackPlugin([
+            'dist',
+        ], {
+            root: process.cwd(),
+        }),
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: '"production"',
