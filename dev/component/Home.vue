4f5bebff0ac171e01bea9cd4c6f7de8be12f537e
JoshuaYang <yss901025@gmail.com>
2017-06-29 15:47:45 +0800

    dynamic router

---
+++ dev/component/Home.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div class="home">
+        <img class="stuff" src="../image/stuff.svg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss">
+.home {
+    .stuff {
+        width: 200px;
+    }
+}
+</style>
--- dev/router.js
+++ dev/router.js
@@ -3,12 +3,26 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+const Home = resolve => require(['./component/Home.vue'], resolve);
+const Controller = resolve => require(['./component/Controller.vue'], resolve);
+const Message = resolve => require(['./component/Message.vue'], resolve);
+
 const routes = [
-    // {
-    //     path: '/',
-    //     name: 'home',
-    //     component: Home,
-    // },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/Controller',
+        name: 'Controller',
+        component: Controller,
+    },
+    {
+        path: '/Message',
+        name: 'Message',
+        component: Message,
+    },
 ];
 
 const router = new VueRouter({
