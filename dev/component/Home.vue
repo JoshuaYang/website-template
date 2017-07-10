0beb42ee2c30778536fcf1037c2e798c4cc7d7c9
JoshuaYang <yss901025@gmail.com>
2017-07-10 10:27:29 +0800

    inject PictureLoader

--- dev/component/Home.vue
+++ dev/component/Home.vue
@@ -1,11 +1,16 @@
 <template lang="html">
     <div class="home">
-        <img class="stuff" src="../image/stuff.svg" alt="">
+        <div class="stuff preload" data-source="../image/stuff.svg"></div>
     </div>
 </template>
 
 <script>
+import PictureLoader from 'joshua-picture-loader';
+
 export default {
+    mounted() {
+        new PictureLoader().load();
+    },
 };
 </script>
 
